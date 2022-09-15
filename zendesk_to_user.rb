001bdbd43c62a21bcbc3ab93f591792deb4c4b09
y.kawarazuka <y.kawarazuka@gmail.com>
2022-09-15 10:28:22 +0900

    modify

--- zendesk_to_user.rb
+++ zendesk_to_user.rb
@@ -10,12 +10,22 @@ class ZendeskParser
     @access_token = access_token
   end
 
-  def pagenate(uri, aggregate_array, aggregate_key = 'results')
-    response = JsonWebApi.get(uri) do |req|
+  def apicall(full_path_or_rel_path_with_params)
+    if full_path_or_rel_path_with_params =~ /^http/
+      uri = full_path_or_rel_path_with_params
+    else
+      uri = "https://#{@subdomain}.zendesk.com/api/v2/#{full_path_or_rel_path_with_params}"
+    end
+
+    JsonWebApi.get(uri) do |req|
       req.basic_auth("#{@email}/token", @access_token)
     end
+  end
+
+  def pagenate(path_with_params, aggregate_array, aggregate_key = 'results')
+    response = apicall(path_with_params)
     aggregate_array = aggregate_array + response[aggregate_key]
-    print "#{aggregate_array.count}/#{response['count']}\n"
+    warn "#{aggregate_array.count}/#{response['count']}\n"
     if response['next_page']
       aggregate_array = pagenate(response['next_page'], aggregate_array, aggregate_key)
     end
@@ -24,14 +34,21 @@ class ZendeskParser
 
   def search(query)
     query = CGI.escape(query)
-    uri = "https://#{@subdomain}.zendesk.com/api/v2/search.json?query=#{query}"
+    rel_path = "search.json?query=#{query}"
     results = []
-    results = pagenate(uri, results, 'results')
+    results = pagenate(rel_path, results, 'results')
     results
   end
 
-  def users_from_search(json_tickets)
-    user_links = json_tickets.map do |result|
+  def ticket_contents(json_search_results)
+    tickets = json_search_results.select{ |r| r['url'] =~ /ticket/ }
+    tickets.map do |ticket|
+      yield(ticket)
+    end
+  end
+
+  def users_from_search(json_search_results)
+    user_links = json_search_results.map do |result|
       media_id = nil
       /admin\/users\/(\d+)/.match(result['notes']) do |m|
         media_id = m[1]
@@ -45,22 +62,38 @@ if __FILE__ == $0
   token = ENV['ZENDESK_ACCESS_TOKEN']
   subdomain = ENV['ZENDESK_SUBDOMAIN']
   email = ENV['ZENDESK_EMAIL']
+  p subdomain
   raise 'ZendeskのAPI Tokenを、環境変数 ZENDESK_ACCESS_TOKEN にセットしてください' unless token
   raise 'Zendeskのsubdomainを、環境変数 ZENDESK_SUBDOMAIN にセットしてください' unless subdomain
   raise 'Zendeskの email を、環境変数 ZENDESK_EMAIL にセットしてください' unless email
   
   z = ZendeskParser.new(subdomain, email, token)
-  search_query = 'notes:"admin/users"'
   if ARGV[0]
     search_query = "#{search_query} #{ARGV[0]}"
   else
-    interval = (Date.today - 3).strftime("%Y-%m-%d")
+    interval = (Date.today - 8).strftime("%Y-%m-%d")
     search_query = "#{search_query} created>#{interval}"
   end
 
   tickets = z.search(search_query)
-  users = z.users_from_search(tickets)
 
-  print users.sort.join(',')
+  contents = z.ticket_contents(tickets) do |ticket|
+    # binding.pry
+    [
+        ticket['id'],
+        ticket['created_at'],
+        ticket['subject'],
+        ticket['description'].gsub(/[\r\n]+/, '<br>'),
+        ticket['tags'].join(',')
+    ].join("\t")
+  end
+
+  contents.each do |content|
+    print content
+    print "\n"
+  end
+
+  # users = z.users_from_search(tickets)
+  # print users.sort.join(',')
   print "\n"
 end
